4fb47e3122015d28e152e5a603d5eaaeb8722df6
avan <132558057+avanavan@users.noreply.github.com>
2025-07-05 03:27:43 +0800

    Update tf.sh

--- tf.sh
+++ tf.sh
@@ -4,4 +4,4 @@ cd $HOME/hlserver
 ./update.sh
 bash update-matcha.sh
 
-tf2/srcds_run -game tf -autoupdate -steam_dir ~/hlserver -steamcmd_script ~/hlserver/tf2_ds.txt $@
+exec tf2/srcds_run -game tf -autoupdate -steam_dir ~/hlserver -steamcmd_script ~/hlserver/tf2_ds.txt $@
