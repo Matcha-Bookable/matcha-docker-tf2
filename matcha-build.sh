cfb785eb215ee2c673bb58ee4415ced7e8e51f10
avan <132558057+avanavan@users.noreply.github.com>
2026-07-30 16:09:23 +0000

    remove deletion of configs folder

--- matcha-build.sh
+++ matcha-build.sh
@@ -129,6 +129,3 @@ date
 rm -f $HOME/hlserver/tf2/tf/addons/sourcemod/plugins/nextmap.smx \
 	  $HOME/hlserver/tf2/tf/addons/sourcemod/plugins/funcommands.smx \
 	  $HOME/hlserver/tf2/tf/addons/sourcemod/plugins/funvotes.smx
-
-# We also don't need any cfg since we provide it in runtime
-rm -rf $HOME/hlserver/tf2/tf/cfg/*
